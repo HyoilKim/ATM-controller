a319ee0cd50c59bfb298f4e48f2c3324d2fe118a
Hyoil Kim <quf9483@naver.com>
2021-07-28 12:51:07 +0900

    update by another changed code

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ from atm import ATM
 if __name__ == "__main__":
     atm = ATM()
 
+    print('======Insert Card======')
     while True:
-        print('======Insert Card======')
         card_number = input('xxxx-xxxx-xxxx-xxxx:')
-        if not atm.validate_card(card_number):
+        if not atm.insert_card(card_number):
             print('{} is not valid card number.\n'.format(card_number)) 
             continue
         else:
@@ -16,7 +16,7 @@ if __name__ == "__main__":
     print('======Insert Pin======')
     while True:
         pin = input('xxxx:')
-        if atm.validate_pin(pin): 
+        if atm.enter_pin(pin): 
             break
         else:
             print('{} is not valid pin.\n'.format(pin))
@@ -33,8 +33,7 @@ if __name__ == "__main__":
     print('======Select Account======')
     while True:
         account_number = input('xxxx-xxxx:')
-        if atm.validate_account(account_number):
-            atm.set_account(account_number)
+        if atm.select_account(account_number):
             break
         else:
             print('{} is not valid account number.\n'.format(account_number))
@@ -43,7 +42,7 @@ if __name__ == "__main__":
 
     print('======Transaction======')
     while True:
-        action = input("Enter action(balance or deposit or withdraw)")
+        action = input("Enter action(balance or deposit or withdraw) or exit:")
         result = None
 
         if action in ['deposit', 'withdraw']:
@@ -51,6 +50,8 @@ if __name__ == "__main__":
             result = atm.transaction(action, money)
         elif action == 'balance':
             result = atm.transaction(action)
+        elif action == 'exit':
+            break
         else:
             print('{} is not valid action.\n'.format(action))
             continue
@@ -59,13 +60,7 @@ if __name__ == "__main__":
         print('======Result=======')
         print('transaction:', result['status'])
         print('current balance:', result['balance'])
-        if result['msg']: print('error msg - ', result['msg'])
-        print('')
-
-        y_or_n = input('Would you like to perform more actions? (y or n):')
-        if y_or_n == 'n': 
-            atm.remove_card()
-            break
+        if result['msg']: print('error msg -', result['msg'])
         print('')
 
     print('')

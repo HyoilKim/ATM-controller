3b44ef6b2a5b0f3c7f6ea3b5b204269a7cc4c887
Hyoil Kim <quf9483@naver.com>
2021-07-30 09:39:20 +0900

    add parameter type, return type

--- account.py
+++ account.py
@@ -1,17 +1,17 @@
 class Account:
-    def __init__(self, account_number, balance=0):
+    def __init__(self, account_number: str, balance=0):
         self.account_number = account_number
         self.balance = balance
     
-    def deposit(self, money: int):
+    def deposit(self, money: int) -> None:
         self.balance += money
 
-    def withdraw(self, money: int):
+    def withdraw(self, money: int) -> bool:
         if money > self.balance:
             return False
         else:
             self.balance -= money  
             return True
 
-    def get_balance(self):
+    def get_balance(self) -> int:
         return self.balance
